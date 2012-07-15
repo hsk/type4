dab568328eeb06508b2415db03d903cff501a102
Hiroshi Sakurai <sakurai777@gmail.com>
2012-07-15 22:55:37 +0900

    change st2ast

--- src/type4/st2ast.scala
+++ src/type4/st2ast.scala
@@ -2,14 +2,23 @@ package type4
 
 object st2ast {
   def main(argv:Array[String]) {
-    val st =
-      (("main","=",("fun","(","void",")",
-                    ("{",("printInt","(",("add","(",(1,",",(2,",",3)),")"),")"),"}")
-          )),
-       "@",
-       ("add","=",("fun","(",("a",",",("b",",","c")),")",
-                   ("a","+",("b","+","c")))
-        ))
+    val st = ( ( (("main",":",(("(","void",")"),"(","void",")")),
+        "=",
+        ("{",(
+          (("printInt","(",("add","(",((1,",",2),",",3),")"),")"),";"),
+          "@",
+           ("printFloat","(",("addf","(",((1.1f,",",0.1f),",",0.2f),")"),")")),
+        "}")),
+        "@",
+        (("add",":",(("(",((("a",":","int"),",",("b",":","int")),",",("c",":","int")),")"),"(","int",")")),
+        "=",
+        ("{",("return",(("a","+","b"),"+","c")),"}"))),
+        "@",
+        (("addf",":",(("(",((("a",":","float"),",",("b",":","float")),",",("c",":","float")),")"),"(","float",")")),
+        "=",
+        ("{",("return",(("a",".+","b"),".+","c")),"}")))
+
+
     val ast = st2ast(st)
     println("ast="+ast)
     val s = setmem(ast)
@@ -23,17 +32,18 @@ object st2ast {
   }
 
   def apply(st:Any):List[Any] = st match {
-    case (a,"@",b) => f(a)::List(f(b))
+    case (a,"@",b) => apply(a):::apply(b)
+    case a => List(f(a))
   }
 
   def f(fn:Any):Any = fn match {
-    case (n,"=",("fun","(",a,")",b)) => ("_"+n, params(a), bodys(b))
+    case ((n,":",(("(",a,")"),"(",t,")")),"=",b) => ("_"+n, params(a):::List(t), bodys(b))
   }
 
   def params(e:Any):List[Any] = e match {
     case (a,",",b) => params(a):::params(b)
-    case "void"=>List()
-    case a => List(a)
+    case (a,":",b) => List((a,b))
+    case "void" => List(("void","void"))
   }
   def fargs(e:Any):List[Any] = e match {
     case (a,",",b) => fargs(a):::fargs(b)
@@ -46,13 +56,15 @@ object st2ast {
     case (a,"(",b,")") => ("call","_"+a,fargs(b))
     case (a,"=",b) => ("mov", exp(b), exp(a))
     case (a,"+",b) => ("add",exp(a), exp(b))
+    case (a,".+",b) => ("addf",exp(a), exp(b))
     case ("return", a) => ("ret", exp(a))
     case (a,";") => exp(a)
     case a:Int => a
     case a:String => a
+    case a:Float => a
   }
   def bodys(e:Any):List[Any] = e match {
-    case (a,"@",b) => bodys(a):::bodys(a)
+    case (a,"@",b) => bodys(a):::bodys(b)
     case a =>
       exp(a) match {
         case e:List[Any] => e
